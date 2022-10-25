b195190fbd4097ad4d92ed6b3cd61a424e3cf22e
Panuwat Khannalae <panumataeh@gmail.com>
2022-10-26 00:24:20 +0700

    - แก้ไข bg

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,22 @@ import Calendar from "./components/Calendar";
 
 function App() {
   return (
-    <div className="container" style={{ backgroundColor: "red" }}>
-      <div
-        className="row justify-content-center align-items-center"
-        style={{ height: "100vh" }}
-      >
-        <div className="text-center col-md-8">
-          <h1 className="mb-3">Countdown to my birthday</h1>
-          <p>
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quas
-            obcaecati atque? Aliquam architecto illum, dignissimos, amet
-            incidunt at expedita quia, ex error molestiae impedit in est.
-            Numquam, soluta dolor.
-          </p>
-          <Calendar />
+    <div style={{ backgroundColor: "red" }}>
+      <div className="container">
+        <div
+          className="row justify-content-center align-items-center"
+          style={{ height: "100vh", width: "100%" }}
+        >
+          <div className="text-center col-md-8">
+            <h1 className="mb-3">Countdown to my birthday</h1>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quo quas
+              obcaecati atque? Aliquam architecto illum, dignissimos, amet
+              incidunt at expedita quia, ex error molestiae impedit in est.
+              Numquam, soluta dolor.
+            </p>
+            <Calendar />
+          </div>
         </div>
       </div>
     </div>
