d0a509e3b5aacefc7e10ac6bbe9199acd4e1d815
Panuwat Khannalae <panumataeh@gmail.com>
2022-10-27 22:47:48 +0700

    - fix bug ( check now date equal Birth day ) - set  year count by current date

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,7 +2,46 @@ import React, { useEffect, useState } from "react";
 import { Col, Container, Row } from "react-bootstrap";
 import CountItem from "./CountItem";
 
-const BIRTH_DAY = new Date("5-8-2023");
+const BIRTH_DAY = new Date("5-8-1999");
+
+const getBirthday = () => {
+  const now = new Date();
+
+  if (now.getMonth() > BIRTH_DAY.getMonth()) {
+    return new Date(
+      now.getFullYear() + 1,
+      BIRTH_DAY.getMonth(),
+      BIRTH_DAY.getDate()
+    );
+  }
+
+  if (now.getMonth() < BIRTH_DAY.getMonth()) {
+    return new Date(
+      now.getFullYear(),
+      BIRTH_DAY.getMonth(),
+      BIRTH_DAY.getDate()
+    );
+  }
+
+  if (now.getMonth() === BIRTH_DAY.getMonth()) {
+    if (now.getDate() > BIRTH_DAY.getDate()) {
+      return new Date(
+        now.getFullYear() + 1,
+        BIRTH_DAY.getMonth(),
+        BIRTH_DAY.getDate()
+      );
+    } else {
+      return new Date(
+        now.getFullYear(),
+        BIRTH_DAY.getMonth(),
+        BIRTH_DAY.getDate()
+      );
+    }
+  }
+
+  // ตัวกันเหนียว
+  return new Date(now.getFullYear(), BIRTH_DAY.getMonth(), BIRTH_DAY.getDate());
+};
 
 const Calendar = () => {
   //state
@@ -15,9 +54,24 @@ const Calendar = () => {
 
   //function input TimeStamp and then return day,hours,minutes and seconds
   const getCountdown = (birthDate) => {
-    let now = new Date().getTime();
+    let now = new Date();
 
-    let timeCount = (birthDate - now) / 1000;
+    if (
+      birthDate.getDate() === now.getDate() &&
+      birthDate.getMonth() === now.getMonth() &&
+      birthDate.getFullYear() === now.getFullYear()
+    ) {
+      return {
+        days: 0,
+        hours: 0,
+        minutes: 0,
+        seconds: 0,
+      };
+    }
+
+    const birthDateTime = birthDate.getTime();
+
+    let timeCount = (birthDateTime - now.getTime()) / 1000;
 
     let days = timeCount / (60 * 60 * 24); //convert seconds to days
     let hours = (timeCount % (60 * 60 * 24)) / (60 * 60); //convert seconds to hours
@@ -33,7 +87,7 @@ const Calendar = () => {
 
   useEffect(() => {
     const intervalTask = setInterval(() => {
-      const dateCount = getCountdown(BIRTH_DAY.getTime());
+      const dateCount = getCountdown(getBirthday());
       setDateCount(dateCount);
     }, 1000); //1000 millisecond = 1 second
     return () => clearInterval(intervalTask);
